ef94d0ec3467c123b9d5c7023720ca9f691640fe
sskywalker <hkrai39@gmail.com>
2019-10-05 20:44:51 +0530

    implemented to lower case feature

---
+++ implementations/toLowerCase.js
@@ -0,0 +1,22 @@
+function toLowerCase(str) {
+    let temp = "";
+for(let chars of str){
+  //Get the ascii value of character
+  let value = chars.charCodeAt();
+
+  //If the character is in uppercase
+  if(value >= 65 && value <= 90){
+    //convert it to lowercase
+    temp += String.fromCharCode(value + 32);
+  }else{
+    //else add the original character
+    temp += chars;
+  }
+}
+    
+}
+
+
+// Demonstration
+// toLowerCase('AbCDEfg');
+// abcdefg
