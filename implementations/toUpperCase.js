162227a127a885fe5bd23d9e807fd41f0651fcb0
RameshSuthar <rameshvirat1182@gmail.com>
2019-10-05 22:21:50 +0530

    Implemented the String.prototype.toUpperCase() function.
    Implemented a function which will convert the lower case string into upper case string.

---
+++ implementations/toUpperCase.js
@@ -0,0 +1,20 @@
+/**
+ *        The String.prototype.toUpperCase() method returns the calling string value converted to uppercase
+ *        (the value will be converted to a string if it isn't one).
+ *
+ *        To Implement this function I have used charCodeAt() to convert the character into it's ASCII value
+ *        and then I subtracted 32 from that ASCII value to convert that ASCII value of lower letter to the ASCII
+ *        value of Upper letter.
+ *        Finally I used String.fromCharCode() to convert the ASCII value into character.
+ * 
+ *  */
+
+function lowerToUpper(str){
+    let upperCaseString = ""
+    for(let i=0;i<str.length;i++){
+        upperCaseString += String.fromCharCode(str.charCodeAt(i) - 32);
+    }
+    return upperCaseString
+}
+
+lowerToUpper('hacktober fest is a one month festival of open source software');
